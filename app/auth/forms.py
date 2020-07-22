9b3703b234117c829e237cafbf565a24872aa494
Kenn <sabatojnr01@gmail.com>
2020-07-22 16:56:26 +0300

    email validation in the forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,7 +14,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
         if User.query.filter_by(email =data_field.data).first():
-            raise ValidationError('Account already exists!')
+            raise ValidationError('The account already exists!')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username =  data_field.data).first():

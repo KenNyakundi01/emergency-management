cbf780ca5d69103d2b6a83bf5612e8988e98d495
kingsam91 <lambetgm@gmail.com>
2020-07-23 08:45:23 -0400

    setup email configurations

--- config.py
+++ config.py
@@ -6,6 +6,16 @@ class Config:
 	'''
 	SQLALCHEMY_TRACK_MODIFICATIONS=False
 
+	    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "beresnasam123@gmail.com"
+    MAIL_PASSWORD = "sams1234"
+    SUBJECT_PREFIX = 'emergency-management'
+    SENDER_EMAIL = 'beresnasam123@gmail.com'
+
+
 class ProdConfig(Config):
     pass
 

e9948b6cc325c411deb6db7f629badedf3854ba6
Noble Mungu <mungunoble@gmail.com>
2020-07-24 07:54:22 +0300

    update auth forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
+from email_validator import validate_email  
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -13,15 +14,15 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
         if User.query.filter_by(email =data_field.data).first():
-            raise ValidationError('The account already exists!')
+            raise ValidationError('Account already exists!')
 
-    
     def validate_username(self,data_field):
         if User.query.filter_by(username =  data_field.data).first():
             raise ValidationError('Username is taken')
 
+
 class LoginForm(FlaskForm):
-    email = StringField('Enter your Email Address',validators=[Required(),Email()])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Password',validators=[Required()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
